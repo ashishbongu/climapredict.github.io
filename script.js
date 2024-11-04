6e565361ecd87c494a6bb2ddb31883bbf83d27e0
ashishbongu <bonguashish532222@gmail.com>
2024-11-04 16:25:09 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Simple cart functionality
-let cart = [];
-
-function addToCart(productName, price) {
-    let product = { name: productName, price: price };
-    cart.push(product);
-    alert(`${productName} has been added to your cart.`);
-    console.log(cart);
-}
